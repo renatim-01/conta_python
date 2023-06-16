c8cc7443a0203c8827018874f5eeac66ec30e636
Renato Lopes <86698086+renatim-01@users.noreply.github.com>
2023-06-16 00:38:58 -0300

    Add files via upload

---
+++ conta.py
@@ -0,0 +1,54 @@
+
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo objeto ... {}".format(self))
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
+        self.__codigo_banco = "001"
+
+    def extrato(self):
+        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel
+        pass
+
+    def saca(self, valor):
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou do limite".format(self.__limite))
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
